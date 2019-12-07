a146324e21dd0733b407f561501e9a2cd2b901fb
arijeet <arijeetsaha02@gmail.com>
2019-12-07 23:58:59 +0530

    Add basic spring cloud

---
+++ src/test/java/com/arijeet/springcloud/cloudmanagement/CloudManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arijeet.springcloud.cloudmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
